1e95a946716b443af4a07ebe6769c14b5cfbaba2
Karol Barbosa <anna.souza.karoline@gmail.com>
2022-09-27 21:08:22 -0300

    Exercicios API Semana 7 Anna Barbosa

--- exercicios/para-casa/exercicios-para-casa.js
+++ exercicios/para-casa/exercicios-para-casa.js
@@ -2,35 +2,35 @@ console.log("\nTentando enviar e-mail...")
 
 //Construção
 
-const acharEmail = (email) => { //constante para achar o email procurado
-    return new Promise((acept, reject) => { //criando uma promisse que dirá se o e-mail foi acept ou rejeict
-        setTimeout(() => { // Tempo para a resposta
+const acharEmail = (email) => { 
+    return new Promise((acept, reject) => { 
+        setTimeout(() => { 
             if (!email) {
-                return reject("\nO e-mail não pode ser vazio.\n\nFalha ao enviar.\n") // MENSAGEM REJECT PARA EMAIL NÃO PREENCHIDO
+                return reject("\nO e-mail não pode ser vazio.\n\nFalha ao enviar.\n") 
             }
-            return acept({ // RETORNANDO EMAIL ACEPT
+            return acept({ 
                 email
             })
-        },3000) // TEMPO DE RESPOSTA DE 3 SEGUNDOS
+        },3000) 
     })
 }
 
-                                        // CONSUMINDO O EMAIL
+                                        
 
-const imprimirEmail = (email) => { // FUNÇÃO DE IMPRESSÃO DO EMAIL
-    acharEmail(email).then((usuaria) => { // PARAMETRO ACHAR EMAIL FEITO E DEPOIS O USUARIA 
+const imprimirEmail = (email) => { 
+    acharEmail(email).then((usuaria) => {  
         console.log(`
             Para: ${usuaria.email}
             Assunto: Resolução de exercício
             ---------------------------------------
             Mensagem: Para conseguir realizar esse exercício será necessário combinar todos os conhecimentos adquiridos em aula... 
-        `); // MENSAGEM QUE SERÁ IMPRESSA 
-        setTimeout(() => { //TEMPO DE RETORNO DA MENSAGEM SUCESS 4 SEGUNDOS
+        `);  
+        setTimeout(() => { 
             console.log("E-mail enviado com sucesso!\n")
         },4000)
-        return {usuaria} // OPERAÇÃO SUCESS, RETORNA O EMAIL DA USUARIA
-    }).catch((err) => { // SE HOUVER ERRO
-        console.error(err) // CONSOLE DE ERRO
+        return {usuaria} 
+    }).catch((err) => { 
+        console.error(err) 
     })
 };
 
